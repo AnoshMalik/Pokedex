274f81daacf341e2f4e7ab140f7b3288a88bbf76
Anosh Malik <anoshmalik.am@gmail.com>
2023-11-27 11:41:14 +0000

    Pokemon api working. Fetching images and names properly

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,30 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState(null);
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonImage, setPokemonImage] = useState("");
+  const [pokemonMoves, setPokemonMoves] = useState("");
+
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:5000/");
-        const result = await response.json();
-        setData(result);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
+      const result = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+      const details = await result.json(); 
+      console.log(details);
+      setPokemonName(details.name);
+      setPokemonImage(details.sprites.other['official-artwork'].front_default);
     };
 
     fetchData();
   }, []);
 
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>React + Express App</h1>
-        <p>{data && data.message} HEYY</p>
-      </header>
+      <h1>POKEDEX</h1>
+      <h2>{pokemonName.toUpperCase()}</h2>
+      <img src={pokemonImage} style={{ width: 300, height: 300 }}></img>
     </div>
   );
 }
