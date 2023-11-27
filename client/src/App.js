cd4bed085cd9e775a55089ac2867bd99c75e93f8
Anosh Malik <anoshmalik.am@gmail.com>
2023-11-27 12:25:18 +0000

    Front end fetching name, hp, moves and type properly

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,21 @@ import "./App.css";
 function App() {
   const [pokemonName, setPokemonName] = useState("");
   const [pokemonImage, setPokemonImage] = useState("");
-  const [pokemonMoves, setPokemonMoves] = useState("");
+  const [pokemonMoves, setPokemonMoves] = useState([]);
+  const [pokemonType, setPokemonType] = useState("");
+  const [pokemonStats, setPokemonStats] = useState();
 
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
-      const details = await result.json(); 
+      const result = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+      const details = await result.json();
       console.log(details);
       setPokemonName(details.name);
       setPokemonImage(details.sprites.other['official-artwork'].front_default);
+      setPokemonType(details.types[0].type.name.toUpperCase());
+      setPokemonStats(details.stats[0].base_stat);
+      setPokemonMoves([details.moves[0].move.name, details.moves[1].move.name, details.moves[2].move.name]);
     };
 
     fetchData();
@@ -25,8 +30,15 @@ function App() {
   return (
     <div className="App">
       <h1>POKEDEX</h1>
-      <h2>{pokemonName.toUpperCase()}</h2>
-      <img src={pokemonImage} style={{ width: 300, height: 300 }}></img>
+      <h2>
+        {pokemonName.toUpperCase()} - {pokemonStats}HP
+      </h2>
+      <img
+        src={pokemonImage}
+        style={{ width: 300, height: 300, border: "solid black 1px" }}
+      ></img>
+      <h2>Type - {pokemonType}</h2>
+      <h3>Moves - {pokemonMoves}</h3>
     </div>
   );
 }
